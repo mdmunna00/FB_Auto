ba1be97019014389baeb0ef98c338bac709c5868
mdmunna00 <55251999+mdmunna00@users.noreply.github.com>
2026-03-08 19:31:14 +0600

    Update useragent.js

--- useragent.js
+++ useragent.js
@@ -14,18 +14,9 @@ function getRandomProfile() {
       timezoneId: "Europe/London"
     },
 
-    {
-      userAgent: "Mozilla/5.0 (Linux; Android 12; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
-      locale: "en-IN",
-      timezoneId: "Asia/Kolkata"
-    },
+  
 
-    {
-      userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
-      locale: "en-US",
-      timezoneId: "America/Los_Angeles"
-    },
-
+    
     {
       userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
       locale: "en-BD",
